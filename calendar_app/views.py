28269351954fcbe5cfcc37b5c78c359e61d7f8ff
Kasai966 <kasai.saika966@gmail.com>
2024-09-30 23:44:07 +0900

    ログイン履歴をカレンダーに表示　を行いました。

--- calendar_app/views.py
+++ calendar_app/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from users.models import LoginRecord
 
 def calendar_view(request):
-    return render(request, 'calendar_app/calendar.html')
+    #ログイン履歴をカレンダーに表示
+    login_records = LoginRecord.objects.filter(user=request.user)
+    events = [{'title':'ログイン', 'start':record.login_date.strftime('%Y-%m-%d'),
+               'color':'green'} for record in login_records]
+    return render(request, 'calendar_app/calendar.html', {'events':events})
