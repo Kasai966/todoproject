e8e463f9d48268b0811fe6a6bac1f78be8862f3e
Kasai966 <kasai.saika966@gmail.com>
2024-09-30 22:33:03 +0900

    ログイン履歴を記録するモデル　作成しました。

--- users/models.py
+++ users/models.py
@@ -3,5 +3,9 @@ from django.contrib.auth.models import AbstractUser
 
 #カスタムユーザーモデルを定義
 class CustomUser(AbstractUser):
-
     pass
+
+#ログイン履歴を記録するモデル
+class LoginRecord(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE) #カスタムユーザーを参照
+    login_date = models.DateTimeField(auto_now_add=True)
