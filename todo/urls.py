f2f029858da92fd662226a9f0f9b548b5a2b55e8
Kasai966 <kasai.saika966@gmail.com>
2024-10-02 17:16:46 +0900

    タスク表示に関するパスを追加しました。

--- todo/urls.py
+++ todo/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from todo.views import TodoMain, TodoList, TodoDetail, TodoCreate, TodoDelete, TodoUpdate
+from . import views
 
 urlpatterns = [
     path('index/', TodoMain.as_view(), name='index'),
@@ -7,6 +8,8 @@ urlpatterns = [
     path('detail/<int:pk>', TodoDetail.as_view(), name='detail'),
     path('create/', TodoCreate.as_view(), name='create'),
     path('delete/<int:pk>', TodoDelete.as_view(), name='delete'),
-    path('update/<int:pk>', TodoUpdate.as_view(), name='update')
+    path('update/<int:pk>', TodoUpdate.as_view(), name='update'),
+    #path('tasks/', views.task_list, name='task_list'),
+    path('tasks/complete/<int:task_id>/',views.complete_task, name='complete_task'),
 ]
 
