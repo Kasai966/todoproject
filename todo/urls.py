4bb2f37c4d58ab164aaecef863afd30ea644f70f
Kasai966 <kasai.saika966@gmail.com>
2024-10-02 13:36:34 +0900

    タスク完了ボタンを追加しました。

--- todo/urls.py
+++ todo/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from todo.views import TodoMain, TodoList, TodoDetail, TodoCreate, TodoDelete, TodoUpdate
 
 urlpatterns = [
-    path('index/', TodoMain.as_view(), name='main'),
+    path('index/', TodoMain.as_view(), name='index'),
     path('list/', TodoList.as_view(), name='list'),
     path('detail/<int:pk>', TodoDetail.as_view(), name='detail'),
     path('create/', TodoCreate.as_view(), name='create'),
